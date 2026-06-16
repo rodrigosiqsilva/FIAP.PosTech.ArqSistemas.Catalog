48da9ce1c6ddb1d55a8bc8c8edc800fe6812ae0f
Rodrigo Siqueira Silva <rodrigosiqueirasilva@hotmail.com>
2026-06-16 08:09:37 -0300

    Ajustes de injeção

--- FIAP.PosTech.ArqSistemas.CatalogWS/Program.cs
+++ FIAP.PosTech.ArqSistemas.CatalogWS/Program.cs
@@ -1,7 +1,9 @@
+using FIAP.PosTech.ArqSistemas.CatalogAPI.Services;
 using FIAP.PosTech.ArqSistemas.CatalogWS.Workers;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<KafkaConsumerWorker>();
+builder.Services.AddSingleton<IOrderGameService, OrderGameService>();
 
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
